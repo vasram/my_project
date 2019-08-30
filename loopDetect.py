8068bae352b5e8ba83a4a728c4d6a9ef65ea31c1
vasram <vasramparmar@gmail.com>
2019-08-30 16:33:46 +0530

    commit four loopdection

--- loopDetect.py
+++ loopDetect.py
@@ -28,8 +28,7 @@ class LinckedList:
             slow_p = slow_p.next
             fast_p = fast_p.next.next
             if slow_p == fast_p:
-                print
-                "Found Loop"
+                print("Found Loop")
                 return
 
 
